d46079d5ab0faa7a1544586cf0720d1a7d545b7c
Husson <axel.husson@smile.fr>
2025-04-09 09:50:13 +0200

    revert dialog class and instead add it to button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -14,7 +14,7 @@ export const Button: React.FC<ButtonProps> = ({
   buttonUtility = '',
   ...props //
 }) => {
-  const buttonClasses = `ring-offset-background focus:ring-ring data-[state=open]:bg-accent data-[state=open]:text-muted-foreground absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4 shared-button-${variant} ${className}`;
+  const buttonClasses = `ring-offset-background focus:ring-ring data-[state=open]:bg-accent data-[state=open]:text-muted-foreground absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 hover:cursor-pointer focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4 shared-button-${variant} ${className}`;
 
   return (
     <button className={buttonClasses} {...props}>
